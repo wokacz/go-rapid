0df89f5d05793f22b268b6a583ea055499ad35bc
Łukasz Wokacz <lukasz@epicup.pl>
2023-08-30 13:20:41 +0200

    Refactor app.go to support multiple HTTP methods and add prefix
    Updated app.go to support not only GET requests but also POST, PUT, DELETE methods. This broadens the app's capability to handle different types of HTTP requests. Also, refactored the handler setup by using lambda functions inside the main methods. Additionally, added a prefix attribute to the App type to manage path prefixes for routes.

--- app.go
+++ app.go
@@ -2,11 +2,13 @@ package rapid
 
 import "net/http"
 
-type HandleFunc func(*Ctx)
-
-type App struct {
-	mux *http.ServeMux
-}
+type (
+	Handler func(*Ctx)
+	App     struct {
+		mux    *http.ServeMux
+		prefix string
+	}
+)
 
 func NewApp() *App {
 	return &App{
@@ -18,7 +20,13 @@ func (a *App) Run(addr string) (err error) {
 	return http.ListenAndServe(addr, a.mux)
 }
 
-func (a *App) GET(pattern string, handler HandleFunc) {
+// HTTP
+
+func createPath() string {
+	return ""
+}
+
+func (a *App) Get(pattern string, handler Handler) {
 	a.mux.HandleFunc(pattern, func(w http.ResponseWriter, r *http.Request) {
 		ctx := &Ctx{
 			Response: w,
@@ -26,4 +34,53 @@ func (a *App) GET(pattern string, handler HandleFunc) {
 		}
 		handler(ctx)
 	})
+}
+
+func (a App) Post(pattern string, handler Handler) {
+	a.mux.HandleFunc(pattern, func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+			return
+		}
+		ctx := &Ctx{
+			Response: w,
+			Request:  r,
+		}
+		handler(ctx)
+	})
+}
+
+func (a *App) Put(pattern string, handler Handler) {
+	a.mux.HandleFunc(pattern, func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPut {
+			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+			return
+		}
+		ctx := &Ctx{
+			Response: w,
+			Request:  r,
+		}
+		handler(ctx)
+	})
+}
+
+func (a *App) Delete(pattern string, handler Handler) {
+
+	a.mux.HandleFunc(pattern, func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodDelete {
+			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+			return
+		}
+		ctx := &Ctx{
+			Response: w,
+			Request:  r,
+		}
+		handler(ctx)
+	})
+}
+
+// PREFIX
+
+func (a *App) Prefix(pattern string, handler Handler) {
+
 }
